0f0154a62aa704601995e344e2d52479832a7be1
wyJoy210 <wyJoy210@gmail.com>
2022-06-21 15:27:26 +0900

    Model 연습

--- mysite/elections/views.py
+++ mysite/elections/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import Candidate
+
 def index(request):
-	return HttpResponse("Hello world")
+	candidates = Candidate.objects.all()
+	str = ''
+	for candidate in candidates:
+		str += "<p>{}  기호{}번({})<br>".format(candidate.name,
+			candidate.party_number, candidate.area)
+		str += candidate.introduction + "</p>"
+
+
+	return HttpResponse(str)
